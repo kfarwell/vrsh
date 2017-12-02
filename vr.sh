f00046484ccc304ec33e5b3a1148014d701a6b64
Kyle Farwell <m@kfarwell.org>
2017-12-02 18:29:51 -0500

    initial commit

---
+++ vr.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+clear
+while read cmd; do
+    echo "$cmd" | bash > /tmp/vrsh
+    
+    height=$(wc -l /tmp/vrsh | cut -d ' ' -f 1)
+    marginh=$(echo $height | awk '{print (66 - $1) / 2}')
+    width=$(wc -L /tmp/vrsh | cut -d ' ' -f 1)
+    marginw=$(echo $width | awk '{print (134 - $1) / 2}')
+    
+    clear
+    
+    for i in $(seq $marginh); do
+        echo
+    done
+    
+    while read line; do
+        #line=$(echo "$line" | sed 's/ *$//')
+    
+        for i in $(seq $marginw); do
+            echo -n ' '
+        done
+    
+        printf '%-'$width's' "$line"
+    
+        for j in $(seq $marginw); do
+            echo -n '  '
+        done
+    
+        printf '%-'$width's' "$line"
+
+        echo
+    done < /tmp/vrsh
+   
+    for i in $(seq $marginw); do
+        echo -n ' '
+    done
+    echo -n '$ '
+done
